e154aa89a1bfcff001ccdf8abdf4aa492c2dccaa
Andrei <gexinator@gmail.com>
2024-11-03 23:18:15 +0300

    Init Commit

---
+++ Scenes/car.gd
@@ -0,0 +1,12 @@
+extends VehicleBody3D
+
+var max_rpm = 80
+var max_torque = 170
+
+func _physics_process(delta: float) -> void:
+	steering = lerp(steering, Input.get_axis("Right","Left") * 0.4, 5 * delta)
+	var acceleration = Input.get_axis("Back", "Forward")
+	var rpm = $rear_left_wheel.get_rpm()
+	$rear_left_wheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
+	rpm = $rear_right_wheel.get_rpm()
+	$rear_right_wheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
